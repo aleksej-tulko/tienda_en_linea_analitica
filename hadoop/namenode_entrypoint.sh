e507714f2c7bdce83842e0d62d04b277ee2ad3da
Aleix Tulko <alexeitulko@gmail.com>
2025-10-18 10:29:21 +0200

    changes

--- hadoop/namenode_entrypoint.sh
+++ hadoop/namenode_entrypoint.sh
@@ -5,7 +5,7 @@ set -e
 mkdir -p /usr/local/hadoop/hdfs/namenode
 chmod -R 777 /usr/local/hadoop/hdfs/namenode
 
-hdfs namenode -format
+hdfs namenode -format -force -nonInteractive
 hdfs --daemon start namenode
 hdfs --daemon start secondarynamenode
 yarn --daemon start resourcemanager

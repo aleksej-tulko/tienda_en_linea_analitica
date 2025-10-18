d761580191069735d6d29c452477657da05b1ded
Aleix Tulko <alexeitulko@gmail.com>
2025-10-18 11:22:39 +0200

    yarn dirs

--- hadoop/datanode_entrypoint.sh
+++ hadoop/datanode_entrypoint.sh
@@ -5,6 +5,9 @@ set -e
 mkdir -p /usr/local/hadoop/hdfs/datanode
 chmod -R 777 /usr/local/hadoop/hdfs/datanode
 
+mkdir -p /opt/hadoop/yarn/local /opt/hadoop/yarn/logs
+chmod -R 777 /opt/hadoop/yarn
+
 yarn --daemon start nodemanager
 
 exec "$@"

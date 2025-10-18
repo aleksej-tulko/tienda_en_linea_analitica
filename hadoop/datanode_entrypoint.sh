83750173f1c34d1e9046e7cfa4ca3850b33a5583
Aleix Tulko <alexeitulko@gmail.com>
2025-10-18 10:21:39 +0200

    change nodes

--- hadoop/datanode_entrypoint.sh
+++ hadoop/datanode_entrypoint.sh
@@ -5,5 +5,7 @@ set -e
 mkdir -p /usr/local/hadoop/hdfs/datanode
 chmod -R 777 /usr/local/hadoop/hdfs/datanode
 
+hdfs --daemon start datanode
+yarn --daemon start nodemanager
 
 exec "$@"
--- hadoop/namenode_entrypoint.sh
+++ hadoop/namenode_entrypoint.sh
@@ -6,4 +6,8 @@ mkdir -p /usr/local/hadoop/hdfs/namenode
 chmod -R 777 /usr/local/hadoop/hdfs/namenode
 
 hdfs namenode -format
+hdfs --daemon start namenode
+hdfs --daemon start secondarynamenode
+yarn --daemon start resourcemanager
+
 exec "$@"

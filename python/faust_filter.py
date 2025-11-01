24415fd8dff16d2a6975485eef109bc801370410
Aleix Tulko <alexeitulko@gmail.com>
2025-11-01 16:11:05 +0100

    no schema

--- python/faust_filter.py
+++ python/faust_filter.py
@@ -187,7 +187,7 @@ app = faust.App(
     consumer_auto_offset_reset="earliest"
 )
 
-goods_topic = app.topic(SHOP_UNSORTED_TOPIC, schema=schema_with_avro)
+goods_topic = app.topic(SHOP_UNSORTED_TOPIC)
 sorted_goods_topic = app.topic(SHOP_SORTED_TOPIC, schema=schema_with_avro)
 
 

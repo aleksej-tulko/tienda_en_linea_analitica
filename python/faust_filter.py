244cba880b921b4df5a21c3ed52a3b0b6b1ddd65
Aleix Tulko <alexeitulko@gmail.com>
2025-11-01 15:39:19 +0100

    print

--- python/faust_filter.py
+++ python/faust_filter.py
@@ -190,6 +190,7 @@ sorted_goods_topic = app.topic(SHOP_SORTED_TOPIC, schema=schema_with_avro)
 @app.agent(goods_topic)
 async def my_agent(stream: faust.Stream[SchemaValue]):
     async for record in stream:
+        print(record)
         await sorted_goods_topic.send(
             value=record
         )

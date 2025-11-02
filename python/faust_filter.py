3c866ab85f393c7e7d28fe6949d64ce98861ad1a
Aleix Tulko <alexeitulko@gmail.com>
2025-11-02 10:45:31 +0100

    item

--- python/faust_filter.py
+++ python/faust_filter.py
@@ -259,7 +259,7 @@ def convert_price(value: SchemaValue) -> SchemaValue:
 
 @app.agent(prohibited_goods_topic, sink=[log_prohibited_items])
 async def filter_prohibited_goods(stream):
-    async for item in stream.group_by(ProhibitedProducts['prohibited']):
+    async for item in stream.group_by(ProhibitedProducts.item):
         filter_table['prohibited'] += item.item
         yield (filter_table['prohibited'])
 

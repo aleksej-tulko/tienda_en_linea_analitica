21a8e63e8b006182ccb2a8ef3fec62e02b44d753
Aleix Tulko <alexeitulko@gmail.com>
2025-11-09 17:17:46 +0100

    dir

--- python/faust_filter.py
+++ python/faust_filter.py
@@ -294,6 +294,8 @@ async def add_filtered_record(stream):
     async for record in processed_stream:
         if not re.match(re_pattern, record.category):
             continue
+        print(dir(filter_table['prohibited']))
+        print(filter_table['prohibited'])
         if record.name in filter_table['prohibited'].item:
             continue
         await sorted_goods_topic.send(

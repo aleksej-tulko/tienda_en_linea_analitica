b81ad338cb17450f289f0c4d33577c15204bd778
Aleix Tulko <alexeitulko@gmail.com>
2025-11-30 16:52:49 +0100

    tasks max

--- python/hdfs_get_data.py
+++ python/hdfs_get_data.py
@@ -3,10 +3,11 @@ from hdfs import InsecureClient
 if __name__ == "__main__":
 
     hdfs_client = InsecureClient("http://100.110.19.157:9870", user="root")
-    partitions = hdfs_client.list('/topics/source.filtered_items/partition=0')
+    partitions = hdfs_client.list('/topics/source.filtered_items/')
 
-    file = f'/topics/source.filtered_items/partition=0/{partitions[0]}'
+    # file = f'/topics/source.filtered_items/'
+    print(partitions)
 
-    with hdfs_client.read(file, encoding="utf-8") as reader:
-        content = reader.read()
-    print(f"Чтение файла '{file}' из HDFS. Содержимое файла: '{content.strip()}'")
+    # with hdfs_client.read(file, encoding="utf-8") as reader:
+    #     content = reader.read()
+    # print(f"Чтение файла '{file}' из HDFS. Содержимое файла: '{content.strip()}'")

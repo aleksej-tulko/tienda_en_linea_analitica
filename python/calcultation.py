ab5daa36f122a6703e1efbaf20292866f9b48a6b
Aleix Tulko <alexeitulko@gmail.com>
2025-12-07 16:56:13 +0100

    change version

--- python/calcultation.py
+++ python/calcultation.py
@@ -3,9 +3,7 @@ from pyspark.sql import SparkSession
 
 conf = (
     pyspark.SparkConf().setAppName('Test spark').setMaster(
-        'spark://100.110.19.157:6969'
+        'spark://100.110.19.157:7077'
     )
 )
 context = pyspark.SparkContext(conf=conf)
-
-# text_file = context.textFile("hdfs://inputFiles/CountOfMonteCristo/BookText.txt")
